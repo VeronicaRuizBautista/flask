61c22c72cf17600f616d3ded2516f2898cd49cca
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-03-30 13:15:48 -0500

    feat: :construction: Pagina index lista
    Se agrego el archivo json y se itero sobre el para las imagenes

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 #flask --app app run --debug
+import requests
 from flask import Flask, render_template
 app = Flask (__name__)
 
-@app.route("/")
+@app.get("/")
 def index():
-    return render_template('index.html')
+    peticion = requests.get("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
+    return render_template('index.html', data=peticion.json())
 
-app = Flask(__name__, static_url_path='/static')
+@app.route("/views/abrigos")
+def abrigos(): 
+    return render_template('abrigos.html')
 
+@app.route("/views/camisetas")
+def camisetas():
+    return render_template('camisetas.html')
 
-app.run()
+@app.route("/views/pantalones")
+def pantalones():
+    return render_template('pantalones.html')
+
+@app.route("/views/carritos")
+def carritos():
+    return render_template('carritos.html')
+
+@app.route("/views/abrir")
+def abrir():
+    return render_template('abrir.html')
+
+@app.route("/views/vaciarcarrito")
+def vaciarcarrito():
+    return render_template('vaciarcarrito.html')
+
+app.run(debug=True)
 
