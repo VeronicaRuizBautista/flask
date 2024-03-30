aee364a818637d844638f4db2f6e00f8cd5d965e
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-03-30 13:31:32 -0500

    feat: :construction: Se termino la paginad de abrigos
    Se agrego la iteracion del json y se borraron los css no necesarios

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ import requests
 from flask import Flask, render_template
 app = Flask (__name__)
 
+peticion = requests.get("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
+
 @app.get("/")
 def index():
-    peticion = requests.get("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
     return render_template('index.html', data=peticion.json())
 
 @app.route("/views/abrigos")
 def abrigos(): 
-    return render_template('abrigos.html')
+    return render_template('abrigos.html', data=peticion.json())
 
 @app.route("/views/camisetas")
 def camisetas():
-    return render_template('camisetas.html')
+    return render_template('camisetas.html', data=peticion.json())
 
 @app.route("/views/pantalones")
 def pantalones():
-    return render_template('pantalones.html')
+    return render_template('pantalones.html', data=peticion.json())
 
 @app.route("/views/carritos")
 def carritos():
